1caeddf505fecd1bdbfb598692deb8e8d84b493f
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-31 23:01:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 
 class CreateCostumeStores
   def change
-    
+
   end
 
 
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,8 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses
+  def change
+    
+  end
+
+
+end
