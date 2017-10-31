fad36a8440cccf99f2e84a6918af81ed596831c5
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-31 23:47:32 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "closing_time"
+    t.datetime "opening_time"
+  end
+
+  create_table "costumes", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.text "image_url"
+    t.string "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
